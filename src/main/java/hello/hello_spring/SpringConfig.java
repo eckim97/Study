c2e058756f27271ed5078e40d7067034250eca5b
kec <weoncheol1@naver.com>
2024-07-02 23:54:40 +0900

    스프링 데이터 JPA <SpringDataJpaMemberRepository> - JpaRepository를 상속받고 있으면 스프링 데이터 JPA가 구현체를 자동으로 스프링 빈에 등록해 준다.
    <SpringConfig>
    - 스프링 데이터 JPA가 만들어놓은 구현체를 주입받아서 사용하면 된다.
    
    <스프링 데이터 JPA 제공 기능>
    - 인터페이스를 통한 기본적인 CRUD
    - findByName(), findByEmail()처럼 메서드 이름 만으로 조회 기능 제공
    - 페이징 기능 자동 제공

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -2,33 +2,32 @@ package hello.hello_spring;
 
 import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository(memberRepository) {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//        this.memberRepository = memberRepository;
+//    }
 }
