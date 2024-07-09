14859b88f8616dfdd539680c266206da24394e13
kec <weoncheol1@naver.com>
2024-07-10 03:15:38 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 해결
    라이브러리를 추가한다.
    implementation 'jakarta.inject:jakarta.inject-api:2.0.1'
    
    실행해보면
    - provider.get()을 통해 항상 새로운 프로토타입 빈이 생성된다.
    - provider의 get()을 호출하면 내부에서 스프링 컨테이너를 통해 해당 빈을 찾아서 반환한다 (DL)
    - 자바 표준이고, 기능이 단순하여 단위 테스트와 mock 코드 작성이 쉽다.
    - Provider는 필요한 DL 기능만 제공한다.
    
    특징
    - get() 메서드 하나로 기능이 매우 단순하다.
    - 별도의 라이브러리가 필요하다.
    - 자바 표준이므로 스프링이 아닌 다른 컨테이너에서도 사용할 수 있다.

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,26 +2,16 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class SIngletonWithPrototypeTest1 {
-
-    @Test
-    void prototypeFind() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        prototypeBean1.addCount();
-        assertThat(prototypeBean1.getCount()).isEqualTo(1);
-
-        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        prototypeBean2.addCount();
-        assertThat(prototypeBean2.getCount()).isEqualTo(1);
-    }
+public class SingletonWithPrototypeTest1 {
 
     @Test
     void singletonClientUsePrototype() {
@@ -35,20 +25,17 @@ public class SIngletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
