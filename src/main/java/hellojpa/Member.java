d3788428f9f0f76854b1149190b3af39fbcb4721
kec <weoncheol1@naver.com>
2024-07-14 21:41:08 +0900

    다대다 [N:M] - 관계형 데이터베이스는 정규화된 테이블 2개로 N:M 관계를 표현할 수 없다.
    - 연결 테이블을 추가해서 1:N, N:1 관계로 풀어내야 한다.
    
    다대다 매핑의 한계
    
    - 편리해 보이지만 실무에서 사용할 수 없다.
    - 연결 테이블이 단순히 연결만 하고 끝나지 않는다.
    - 중간 테이블에 주문시간, 수량 같은 추가 데이터를 넣을 수 없다.
    - 중간 테이블이 숨겨져 있어 쿼리가 복잡하게 작성된다.
    
    다대다 한계 극복
    - 연결 엔티티를 하나 추가한다.
    
    -  @ManyToMany -> @OneToMany, @ManyToOne

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,6 +1,9 @@
 package hellojpa;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Member {
 
@@ -18,6 +21,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
