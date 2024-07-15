dc377cdc30b8e330963d2feab420f53f340a23ce
kec <weoncheol1@naver.com>
2024-07-16 06:58:31 +0900

    기본값 타입
    1. 엔티티 타입
    - @Entity로 정의하는 객체이다.
    
    - 데이터가 변해도 식별자로 지속해서 추적 가능하다.
    - 예를 들어, 회원 엔티티의 키나 나이 값이 변경되어도 식별자로 인식할 수 있다.
    
    2. 값 타입
    - int, Integer, String처럼 단순히 값으로 사용하는 자바 기본 타입이나 객체를 말한다.
    
    - 식별자가 없고 값만 있으므로 변경시 추적이 불가능하다.
    
    - 예를 들어, 숫자 100을 200으로 변경하면 완전히 다른 값으로 대체된다.
    
    임베디드 타입
    - @Embeddable : 값 타입을 정의하는 곳에 표시한다.
    - @Embedded : 값 타입을 사용하는 곳에 표시한다.
    - 기본 생성자는 필수이다.
    
    - 임베디드 타입은 엔티티의 값일 뿐이다.
    
    - 임베디드 타입을 사용하기 전과 후에 매핑하는 테이블은 같다.
    
    - 객체와 테이블을 아주 세밀하게(find-grained) 매핑하는 것이 가능하다.
    
    - 잘 설계한 ORM 애플리케이션은 매핑한 테이블의 수보다 클래스의 수가 더 많다.

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,18 +2,31 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
-public class Member extends BaseEntity{
+public class Member {
 
-    @Id @GeneratedValue
-    @Column(name ="MEMBER_ID")
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn
-    private Team team;
+    // 기간
+    @Embedded
+    private Period workPeriod;
+
+    // 주소
+    @Embedded
+    private Address homeAddress;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
 
     public Long getId() {
         return id;
@@ -31,11 +44,27 @@ public class Member extends BaseEntity{
         this.username = username;
     }
 
-    public Team getTeam() {
-        return team;
+    public Period getWorkPeriod() {
+        return workPeriod;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Address getWorkAddress() {
+        return workAddress;
+    }
+
+    public void setWorkAddress(Address workAddress) {
+        this.workAddress = workAddress;
     }
 }
