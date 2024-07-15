aa43426aee88411797667da8d6b522b7212b84be
kec <weoncheol1@naver.com>
2024-07-16 01:53:46 +0900

    프록시
    - em.find() : 데이터베이스를 통해서 실제 엔티티 객체 조회
    
    - em.getReference() : 데이터베이스 조회를 미루는 가짜(프록시) 엔티티 객체 조회
    
    - 프록시 객체는 처음 사용할 때 한 번만 초기화 된다.
    
    - 프록시 객체를 초기화 할 때 프록시 객체가 실제 엔티티로 바뀌는 것이 아니다. 초기화 되면 프록시 객체를 통해서 실제 엔티티에 접근이 가능해지는 것이다.
    
    -  영속성 컨텍스트에 찾는 엔티티가 이미 있으면 em.getReference()를 호출해도 실제 엔티티를 반환한다.
    
    - 위와 반대로 프록시로 먼저 Member를 찾고 그다음 실제 객체를 호출해도 프록시를 반환한다.
    
    - 영속성 컨텍스트의 도움을 받을 수 없는 준영속 상태일 때, 프록시를 초기화하면 문제가 발생한다.

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,10 +1,6 @@
 package hellojpa;
 import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 public class Member extends BaseEntity{
 
@@ -18,13 +14,6 @@ public class Member extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
     private Team team;
-
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
-
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -40,5 +29,13 @@ public class Member extends BaseEntity{
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
