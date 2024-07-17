a1d0e907258c37bf1bab28e4728303c807582af6
kec <weoncheol1@naver.com>
2024-07-18 02:34:57 +0900

    값 타입 컬렉션 - 값 타입을 하나 이상 저장할 때 사용한다.
    - @ElementCollection, @CollectionTable을 사용한다.
    
    - 데이터베이스는 컬렉션을 같은 테이블에 저장할 수 없다.
    
    - 컬렉션을 저장하기 위한 별도의 테이블이 필요하다.
    
    값 타입 컬렉션의 제약사항
    
    - 식별자 없음: 값 타입은 엔티티와 달리 식별자 개념이 없어서 변경 시 추적이 어렵다.
    
    - 변경 사항 처리: 값 타입 컬렉션에 변경 사항이 발생하면 주인 엔티티와 연관된 모든 데이터를 삭제하고, 현재 값을 모두 다시 저장한다.
    
    - 기본 키 구성: 값 타입 컬렉션을 매핑하는 테이블은 모든 컬럼을 묶어서 기본 키를 구성해야 하며, null 입력 및 중복 저장이 불가능하다.
    
    정리
    
    엔티티 타입의 특징
    - 식별자가 있다.
    - 생명 주기 관리가 된다.
    - 공유할 수 있다.
    
    값 타입 특징
    - 식별자가 없다.
    - 생명 주기를 엔티티에 의존한다.
    - 공유하지 않는 것이 안전하다(복사해서 사용)
    - 불변 객체로 만드는 것이 안전하다.

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,9 @@
 package hellojpa;
 import jakarta.persistence.*;
 
+import java.util.List;
+import java.util.Set;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -13,16 +16,37 @@ public class JpaMain {
 
         try {
 
-            Address address = new Address("city", "street", "10000");
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setHomeAddress(address);
-            em.persist(member1);
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
 
-            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipCode());
-            member1.setHomeAddress(newAddress);
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
 
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("=====================START===================");
+            Member findMember = em.find(Member.class, member.getId());
+
+            // homeCity -> newCity
+            // findMember.getHomeAddress().setCity("newCity"); // X
+
+//            Address a = findMember.getHomeAddress();
+//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipCode()));
+
+            //치킨 -> 한식
+//            findMember.getFavoriteFoods().remove("치킨");
+//            findMember.getFavoriteFoods().add("한식");
+
+//            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "10000"));
+//            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "10000"));
 
             tx.commit();
         } catch (Exception e) {
