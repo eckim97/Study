5c0af6d9c87e521f094a4e0e63a967c6977020d2
kec <weoncheol1@naver.com>
2024-07-20 01:39:19 +0900

    경로 표현식 .(점)을 찍어 객체 그래프를 탐색하는 것
    경로 표현식 용어 정리
    
    - 상태 필드(state field): 단순히 값을 저장하기 위한 필드 (ex: m.username)
    
    - 연관 필드(association field): 연관관계를 위한 필드
     ㄴ 단일 값 연관 필드:  @ManyToOne, @OneToOne, 대상이 엔티티(ex: m.team)
     ㄴ 컬렉션 값 연관 필드: @OneToMany, @ManyToMany, 대상이 컬렉션(ex: m.orders)
    
     경로 탐색을 사용한 묵시적 조인 시 주의사항
    
     - 항상 내부 조인
    
     - 컬렉션은 경로 탐색의 끝, 명시적 조인을 통해 별칭을 얻어야 한다.
    
     - 경로 탐색은 주로 SELECT, WHERE 절에서 사용하지만 묵시적 조인으로 인해 SQL의 FROM (JOIN) 절에 영향을 준다.
    
     결론 : 묵시적 조인 쓰지말고 명시적 조인을 쓰자.

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -5,6 +5,7 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -18,32 +19,28 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team1 = new Team();
-            team1.setName("TeamA");
-            em.persist(team1);
+            Team team = new Team();
+            em.persist(team);
 
             Member member1 = new Member();
             member1.setUsername("관리자1");
-            member1.setTeam(team1);
-            team1.getMembers().add(member1);
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
-            member2.setTeam(team1);
-            team1.getMembers().add(member2);
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select function('group_concat', m.username) From Member m";
-            List<String> result = em.createQuery(query, String.class)
+            String query = "select m. From Team t join t.members m";
+
+            Collection result = em.createQuery(query, Collection.class)
                     .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e) {
