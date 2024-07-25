7575b5ab1f8128d449ca0b031b0135833f645909
kec <weoncheol1@naver.com>
2024-07-25 23:41:46 +0900

    객체지향 프로그래밍 실습 - 학점계산기
    일급 컬렉션을 사용하여 GradeCalculator에 남아있는 코스에 대한 로직 처리를 위임했다.
    
    학점 계산기의 협력 흐름
    
    1. 이수한 과목을 전달하여 평균 학점 계산 요청 (GradeCalculatorTest)
    
    2. 학점을 처리할 학점 계산기 생성 (GradeCalculator.calculateGrade())
    
    3. 학점 계산기에서 총학점(학점수x교과목 평점)과 수강 신청 총학점 계산을 일급 컬렉션에게 요청 (Courses)
    
    <정리>
    객체들이 메시지를 통해 서로 작업을 위임하며, 각 객체가 자신이 가진 값을 처리하도록 설계된 객체지향 프로그램을 구현했다.

--- src/main/java/org/example/GradeCalculator.java
+++ src/main/java/org/example/GradeCalculator.java
@@ -3,23 +3,20 @@ package org.example;
 import java.util.List;
 
 public class GradeCalculator {
-    private final List<Course> courses;
+
+    private final Courses courses;
 
     public GradeCalculator(List<Course> courses) {
-        this.courses = courses;
+        this.courses = new Courses(courses);
     }
 
     public double calculateGrade() {
         // (학점수 x 교과목 평점)의 합계
-        double multipliedCreditAndCourseGrade  = 0;
-        for (Course course : courses) {
-            multipliedCreditAndCourseGrade += course.multiplyCreditAndCourseGrade();
-        }
+        double totalMultipliedCreditAndCourseGrade = courses.multiplyCreaditAndCourseGrade();
 
         // 수강신청 총학점 수
-        int totalCompletedCredit = courses.stream()
-                .mapToInt(Course::getCredit)
-                .sum();
-        return multipliedCreditAndCourseGrade / totalCompletedCredit;
+        int totalCompletedCredit = courses.calculateTotalCompletedCreadit();
+
+        return totalMultipliedCreditAndCourseGrade / totalCompletedCredit;
     }
 }
