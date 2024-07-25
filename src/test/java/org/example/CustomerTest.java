613c849daed09a1040db7beea3a563438b816125
kec <weoncheol1@naver.com>
2024-07-26 01:54:47 +0900

    음식점에서 음식 주문하는 과정
    손님 클래스와 테스트케이스를 작성했다. 이로써 음식점에서 음식을 주문할 때 필요한 코드는 모두 작성하였다.
    
    [전체 흐름]
    
    1. 손님의 방문 (Customer)
    
    손님이 식당에 방문하여 메뉴를 보고 "돈까스"를 주문하고자 한다.
    
    2. 메뉴 확인 (Menu)
    
    메뉴판에는 여러 메뉴 항목(MenuItem)이 나열되어 있으며, 각 메뉴는 이름과 가격 정보를 가지고 있다.
    
    3. 주문 요청 (Customer.order)
    
    손님은 메뉴를 주문하고, 서버는 Customer 객체의 order 메서드를 통해 주문을 처리한다.
    
    4. 메뉴 선택 (Menu.choose)
    
    서버는 Menu 객체의 choose 메서드를 사용하여 주문한 메뉴 항목을 찾는다.
    
    5. 요리 준비 (Cooking.makeCook)
    
    주방은 Cooking 객체의 makeCook 메서드를 사용해 선택된 MenuItem을 기반으로 Cook 객체를 생성하여 요리를 준비한다.
    
    6. 요리 제공
    
    준비된 요리가 손님에게 제공되어, 손님은 주문한 음식을 즐길 수 있다.

--- src/test/java/org/example/CustomerTest.java
+++ src/test/java/org/example/CustomerTest.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 /**
  * 음식점에서 음식 주문하는 과정 구현
@@ -15,7 +20,13 @@ import org.junit.jupiter.api.Test;
 public class CustomerTest {
 
     @Test
-    void name() {
+    @DisplayName("메뉴 이름에 해당하는 요리를 주문한다.")
+    void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        Cooking cooking = new Cooking();
 
+        assertThatCode(() -> customer.order("돈까스", menu, cooking))
+                .doesNotThrowAnyException();
     }
 }
