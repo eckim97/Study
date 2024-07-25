647e820c6705a4b2c5a58541fc0dd81f83cdaa1a
kec <weoncheol1@naver.com>
2024-07-26 01:17:39 +0900

    음식점에서 음식 주문하는 과정
    메뉴판에 없는 메뉴를 선택할 시 예외를 반환하는 코드를 작성하고 검증했다.

--- src/test/java/org/example/MenuTest.java
+++ src/test/java/org/example/MenuTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class MenuTest {
 
@@ -17,5 +18,14 @@ public class MenuTest {
 
         assertThat(menuItem).isEqualTo(new MenuItem("돈까스", 5000));
 
+    }
+
+    @Test
+    @DisplayName("메뉴판에 없는 메뉴를 선택할 시 예외를 반환한다.")
+    void chooseTest2() {
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        assertThatCode(() -> menu.choose("치킨"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 메뉴 이름입니다.");
     }
 }
