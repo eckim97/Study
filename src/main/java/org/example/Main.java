b648032647d2af1e19ced22d2f5ffb4b3bcfb52f
kec <weoncheol1@naver.com>
2024-07-27 01:42:20 +0900

    계산기 웹 프로그램 구현하기 - 1 <요구사항> 1. 사용자 요청을 메인 Thread가 처리하도록 한다. 2. 사용자 요청이 들어올 때 마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다. 3. Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
    CustomWebApplicationServer로 서버를 간단하게 구현하고 클라이언트가 GET 요청을 했을 때 어떤 프로토콜을 넘기는지 line을 출력하면서 확인해보았다. 즉 톰켓이 어떤 역할을 하는지 알아보며 개발 진행 중이다.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        new CustomWebApplicationServer(8080).start();
+
+
     }
 }
