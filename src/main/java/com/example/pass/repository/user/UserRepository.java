a58bf41c4840bbd77fb8463b3b08ce469946e124
kec <weoncheol1@naver.com>
2024-08-27 22:27:07 +0900

    이용권 일괄 지급을 위한 Batch Job, Tasklet, Entity 및 테스트 코드 추가 이용권 일괄 지급을 위한 Batch Job 설정 및 Tasklet 구현.
    BulkPassEntity, PassEntity, UserGroupMappingEntity와 관련된 Entity 및 Repository 추가.
    
    MapStruct를 활용한 매핑 로직과 Tasklet 동작에 대한 테스트 코드 작성.

--- src/main/java/com/example/pass/repository/user/UserRepository.java
+++ src/main/java/com/example/pass/repository/user/UserRepository.java
@@ -2,5 +2,5 @@ package com.example.pass.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 }
