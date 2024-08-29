253d525b73c5161e277be9e2c99732a5d11d8859
kec <weoncheol1@naver.com>
2024-08-30 05:14:49 +0900

    DB 연동 설정 및 테스트 완료
    build.gradle에 MySQL 연동 및 JPA 의존성 추가
    
    Member 엔티티 및 MemberRepository 생성
    
    application.yml에 데이터소스 설정 추가 (URL, 사용자명, 비밀번호, 드라이버 클래스 등)
    
    DB와 연동된 save 및 find 기능 구현 및 테스트
    
    ddl-auto: create 설정으로 인해 테이블 드랍 및 생성 테스트 (운영 환경에서는 none으로 설정 권장)
    
    테스트 결과 DB에 데이터 정상적으로 저장 및 조회 확인

--- module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
+++ module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
@@ -1,6 +1,7 @@
 package dev.be.moduleapi.controller;
 
 import dev.be.moduleapi.service.DemoService;
+import dev.be.modulecommon.repositories.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
