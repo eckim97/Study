4b5761ab530827d9572a137d88520e3a7b271df1
kec <weoncheol1@naver.com>
2024-09-08 01:17:14 +0900

    feat: 로그인 응답 객체 추가
    - UserLoginResponse: 로그인 성공 시 JWT 토큰을 포함한 응답 처리 클래스 추가

--- src/main/java/com/example/sns/controller/response/UserLoginResponse.java
+++ src/main/java/com/example/sns/controller/response/UserLoginResponse.java
@@ -7,16 +7,6 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class UserJoinResponse {
-    private Integer id;
-    private String userName;
-    private UserRole role;
-
-    public static UserJoinResponse fromUser(User user) {
-        return new UserJoinResponse(
-                user.getId(),
-                user.getUserName(),
-                user.getUserRole()
-        );
-    }
+public class UserLoginResponse {
+    private String token;
 }
