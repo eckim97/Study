1aa9ee82d6b015d0302ded9362a63dbbad4c5609
kec <weoncheol1@naver.com>
2024-09-08 20:19:55 +0900

    feat: 포스트 수정 기능 구현
    - PostService에 포스트 수정 로직 추가
    - 사용자 권한 검증 및 포스트 존재 여부 확인

--- src/main/java/com/example/sns/service/PostService.java
+++ src/main/java/com/example/sns/service/PostService.java
@@ -24,4 +24,15 @@ public class PostService {
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
         postEntityRepository.save(PostEntity.of(title, body, userEntity));
     }
+
+    @Transactional
+    public void modify(String title, String body, String userName, Integer postId) {
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+
+        // post exist
+
+
+        // post permission
+    }
 }
