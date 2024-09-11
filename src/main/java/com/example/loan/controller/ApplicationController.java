124cb58bf38d8e0d5a80f6f256b1f26f8da2d27a
kec <weoncheol1@naver.com>
2024-09-11 21:04:01 +0900

    feat: ApplicationController에 대출 신청 등록 기능 추가
    대출 신청 등록 요청을 처리하는 POST 메서드 추가.
    ApplicationService를 호출하여 대출 신청 정보를 처리 후 응답 반환.

---
+++ src/main/java/com/example/loan/controller/ApplicationController.java
@@ -0,0 +1,25 @@
+package com.example.loan.controller;
+
+import com.example.loan.dto.ApplicationDTO;
+import com.example.loan.dto.ResponseDTO;
+import com.example.loan.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.example.loan.dto.ApplicationDTO.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/applications")
+public class ApplicationController extends AbstractController {
+
+    private final ApplicationService applicationService;
+
+    @PostMapping
+    public ResponseDTO<Response> create(@RequestBody Request request) {
+        return ok(applicationService.create(request));
+    }
+}
