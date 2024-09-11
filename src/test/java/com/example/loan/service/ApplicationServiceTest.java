752e4cbf3246adbab40849b633e9717c4b935142
kec <weoncheol1@naver.com>
2024-09-11 21:48:18 +0900

    test: Application 조회 기능 테스트 추가
    특정 applicationId로 대출 신청 정보를 조회하는 단위 테스트 작성.
    Mockito를 사용하여 조회한 정보가 올바르게 응답되는지 검증.

--- src/test/java/com/example/loan/service/ApplicationServiceTest.java
+++ src/test/java/com/example/loan/service/ApplicationServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static com.example.loan.dto.ApplicationDTO.Request;
 import static com.example.loan.dto.ApplicationDTO.Response;
@@ -52,6 +53,20 @@ class ApplicationServiceTest {
 
         assertThat(actual.getHopeAmount()).isSameAs(entity.getHopeAmount());
         assertThat(actual.getName()).isSameAs(entity.getName());
+    }
+
+    @Test
+    void Should_ReturnResponseOfExistingApplicationEntity_when_RequestExistApplicationId() {
+        Long findId = 1L;
+
+        Application entity = Application.builder()
+                .applicationId(1L)
+                .build();
+        when(applicationRepository.findById(findId)).thenReturn(Optional.ofNullable(entity));
+
+        Response actual = applicationService.get(findId);
+
+        assertThat(actual.getApplicationId()).isSameAs(findId);
     }
 
 }
