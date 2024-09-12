ffd3a529c28215f3a2287ddf5c0dc8b4c4b6d969
kec <weoncheol1@naver.com>
2024-09-13 07:09:17 +0900

    feat: 대출 집행 및 잔고 관리 관련 DTO 추가
    - BalanceDTO: 대출 잔고 정보를 관리하는 DTO 추가
    - EntryDTO: 대출 집행 관련 요청 및 응답을 처리하는 DTO 추가

---
+++ src/main/java/com/example/loan/dto/BalanceDTO.java
@@ -0,0 +1,40 @@
+package com.example.loan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class BalanceDTO implements Serializable {
+
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    @Service
+    public static class Request{
+
+        private Long applicationId;
+
+        private BigDecimal entryAmount;
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    @Service
+    public static class Response{
+
+        private Long balanceId;
+
+        private Long applicationId;
+
+        private BigDecimal balance;
+    }
+}
