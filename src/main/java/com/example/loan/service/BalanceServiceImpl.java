98fe33f4b4742087472ed14fb31d0dfe349a98e3
kec <weoncheol1@naver.com>
2024-09-13 07:09:41 +0900

    feat: 대출 잔고 관리 로직 구현
    - BalanceService: 대출 잔고 생성을 위한 인터페이스 추가
    - BalanceServiceImpl: 잔고 관리 로직 구현 (신규 잔고 생성 및 중복 확인)

---
+++ src/main/java/com/example/loan/service/BalanceServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.loan.service;
+
+import com.example.loan.domain.Balance;
+import com.example.loan.exception.BaseException;
+import com.example.loan.exception.ResultType;
+import com.example.loan.repository.BalanceRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+import static com.example.loan.dto.BalanceDTO.Request;
+import static com.example.loan.dto.BalanceDTO.Response;
+
+@Service
+@RequiredArgsConstructor
+public class BalanceServiceImpl implements BalanceService{
+
+    private final BalanceRepository balanceRepository;
+
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Response create(Long applicationId, Request request) {
+        if (balanceRepository.findByApplicationId(applicationId).isPresent()) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+
+        Balance balance = modelMapper.map(request, Balance.class);
+
+        BigDecimal entryAmount = request.getEntryAmount();
+        balance.setApplicationId(applicationId);
+        balance.setBalance(entryAmount);
+
+        Balance saved = balanceRepository.save(balance);
+
+        return modelMapper.map(saved, Response.class);
+    }
+}
