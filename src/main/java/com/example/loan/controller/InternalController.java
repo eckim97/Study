e98556e9ecbc717abc62eeec1eb34c2c9f5a2529
kec <weoncheol1@naver.com>
2024-09-13 07:09:09 +0900

    feat: InternalController에 대출 집행 기능 추가
    - POST /internal/applications/{applicationId}/entries 경로 추가
    - 대출 집행 요청 처리 메서드 추가

---
+++ src/main/java/com/example/loan/controller/InternalController.java
@@ -0,0 +1,22 @@
+package com.example.loan.controller;
+
+
+import com.example.loan.dto.ResponseDTO;
+import com.example.loan.service.EntryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import static com.example.loan.dto.EntryDTO.Request;
+import static com.example.loan.dto.EntryDTO.Response;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/internal/applications")
+public class InternalController extends AbstractController{
+    private final EntryService entryService;
+
+    @PostMapping("{applicationId}/entries")
+    public ResponseDTO<Response> create(@PathVariable Long applicationId, @RequestBody Request request) {
+        return ok(entryService.create(applicationId, request));
+    }
+}
