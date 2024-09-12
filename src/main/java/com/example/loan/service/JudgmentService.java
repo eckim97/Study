0c3d5a8acf9f7f47030ac9a6d5c54467ca80d8e4
kec <weoncheol1@naver.com>
2024-09-13 03:34:36 +0900

    feat: 승인 금액 부여 서비스 메서드 구현
    JudgmentService 및 JudgmentServiceImpl에 심사 정보를 기반으로 승인 금액을 대출 신청에 반영하는 로직 추가.

--- src/main/java/com/example/loan/service/JudgmentService.java
+++ src/main/java/com/example/loan/service/JudgmentService.java
@@ -1,6 +1,8 @@
 package com.example.loan.service;
 
 
+import com.example.loan.dto.ApplicationDTO;
+
 import static com.example.loan.dto.JudgmentDTO.*;
 
 public interface JudgmentService {
@@ -14,5 +16,7 @@ public interface JudgmentService {
     Response update(Long judgmentId, Request request);
 
     void delete(Long judgmentId);
+
+    ApplicationDTO.GrantAmount grant(Long judgmentId);
 }
 
