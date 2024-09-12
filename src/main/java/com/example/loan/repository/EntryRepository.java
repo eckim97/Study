583bfe6a68fab058bacd69300c51b193039112c2
kec <weoncheol1@naver.com>
2024-09-13 07:40:38 +0900

    feat: EntryRepository에 대출 집행 조회 기능 추가
    - applicationId로 대출 집행 내역 조회하는 메서드 추가

--- src/main/java/com/example/loan/repository/EntryRepository.java
+++ src/main/java/com/example/loan/repository/EntryRepository.java
@@ -4,7 +4,10 @@ import com.example.loan.domain.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @RequestMapping
 public interface EntryRepository extends JpaRepository<Entry, Long> {
 
+    Optional<Entry> findByApplicationId(Long applicationId);
 }
