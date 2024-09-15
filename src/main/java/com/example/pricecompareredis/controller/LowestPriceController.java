52d197c9882a43fc1fc22853ad76f23371e45828
kec <weoncheol1@naver.com>
2024-09-16 01:10:50 +0900

    feat: 상품 그룹 관리 및 조회 엔드포인트 추가
    LowestPriceController에 상품 그룹을 추가하고 키워드를 기반으로 상품 그룹을 관리하는 API 엔드포인트를 추가한다.

--- src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
+++ src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
@@ -1,7 +1,9 @@
 package com.example.pricecompareredis.controller;
 
 import com.example.pricecompareredis.service.LowestPriceService;
+import com.example.pricecompareredis.vo.Keyword;
 import com.example.pricecompareredis.vo.Product;
+import com.example.pricecompareredis.vo.ProductGrp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +18,26 @@ public class LowestPriceController {
 
     @GetMapping("/product")
     public Set GetZsetValue(String key) {
-        return mlps.getZsetValue(key);
+        return mlps.GetZsetValue(key);
     }
 
     @PutMapping("/product")
     public int SetNewProduct(@RequestBody Product newProduct) {
-        return mlps.setNewProduct(newProduct);
+        return mlps.SetNewProduct(newProduct);
+    }
+
+    @PutMapping("/productGroup")
+    public int SetNewProduct(@RequestBody ProductGrp newProductGrp) {
+        return mlps.SetNewProductGrp(newProductGrp);
+    }
+
+    @PutMapping("/productGroupToKeyword")
+    public int SetNewProductGrpToKeyword(String keyword, String prodGrpId, double score) {
+        return mlps.SetNewProductGrpToKeyword(keyword, prodGrpId, score);
+    }
+
+    @GetMapping("/productPrice/lowest")
+    public Keyword GetLowestPriceProductByKeyword(String keyword) {
+        return mlps.GetLowestPriceProductByKeyword(keyword);
     }
 }
