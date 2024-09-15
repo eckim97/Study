813f24102445f27b7481da39ecd67d547b4f72d3
kec <weoncheol1@naver.com>
2024-09-16 04:42:54 +0900

    feat: 상품 정보 매핑 및 조회 로직 최적화
    LowestPriceServiceImpl에서 상품 정보 조회 및 매핑 로직을 개선하여 성능 향상과 데이터 처리 정확성 증가.

--- src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
+++ src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
@@ -3,14 +3,12 @@ package com.example.pricecompareredis.service;
 import com.example.pricecompareredis.vo.Keyword;
 import com.example.pricecompareredis.vo.Product;
 import com.example.pricecompareredis.vo.ProductGrp;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class LowestPriceServiceImpl implements LowestPriceService{
@@ -51,18 +49,15 @@ public class LowestPriceServiceImpl implements LowestPriceService{
         return rank;
     }
 
-    @Override
     public Keyword GetLowestPriceProductByKeyword(String keyword) {
         Keyword returnInfo = new Keyword();
         List<ProductGrp> tempProdGrp = new ArrayList<>();
-
         // keyword 를 통해 ProductGroup 가져오기 (10개)
         tempProdGrp = GetProdGrpUsingKeyword(keyword);
 
         // 가져온 정보들을 Return 할 Object 에 넣기
         returnInfo.setKeyword(keyword);
         returnInfo.setProductGrpList(tempProdGrp);
-        
         // 해당 Object return
         return returnInfo;
     }
@@ -70,18 +65,43 @@ public class LowestPriceServiceImpl implements LowestPriceService{
     public List<ProductGrp> GetProdGrpUsingKeyword(String keyword) {
 
         List<ProductGrp> returnInfo = new ArrayList<>();
-        ProductGrp tempProdGrp = new ProductGrp();
 
         // input 받은 keyword 로 productGrpId를 조회
         List<String> prodGrpIdList = new ArrayList<>();
-        prodGrpIdList = List.copyOf(myProdPriceRedis.opsForZSet().range(keyword, 0, 9));
+        prodGrpIdList = List.copyOf(myProdPriceRedis.opsForZSet().reverseRange(keyword, 0, 9));
+        //Product tempProduct = new Product();
+        List<Product> tempProdList = new ArrayList<>();
 
+        // 10개 prodGrpId로 loop
         for (final String prodGrpId : prodGrpIdList) {
-            // Loop 돌면서 ProductGroupID 로 Product:price 가져오기 (10개)
+            // Loop 타면서 ProductGrpID 로 Product:price 가져오기 (10개)
+
+            ProductGrp tempProdGrp = new ProductGrp();
+
             Set prodAndPriceList = new HashSet();
             prodAndPriceList = myProdPriceRedis.opsForZSet().rangeWithScores(prodGrpId, 0, 9);
+            Iterator<Object> prodPricObj = prodAndPriceList.iterator();
+
+            // loop 타면서 product obj에 bind (10개)
+            while (prodPricObj.hasNext()) {
+                ObjectMapper objMapper = new ObjectMapper();
+                // {"value":00-10111-}, {"score":11000}
+                Map<String, Object> prodPriceMap = objMapper.convertValue(prodPricObj.next(), Map.class);
+                Product tempProduct = new Product();
+                // Product Obj bind
+                tempProduct.setProductId(prodPriceMap.get("value").toString()); // prod_id
+                tempProduct.setPrice(Double.valueOf(prodPriceMap.get("score").toString()).intValue()); //es 검색된 score
+                tempProduct.setProdGrpId(prodGrpId);
+
+                tempProdList.add(tempProduct);
+            }
+            // 10개 product price 입력완료
+            tempProdGrp.setProdGrpId(prodGrpId);
+            tempProdGrp.setProductList(tempProdList);
+            returnInfo.add(tempProdGrp);
         }
 
         return returnInfo;
     }
+
 }
