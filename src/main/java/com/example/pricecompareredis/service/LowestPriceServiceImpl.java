c991997500fb6bf11c8ccb82af4d05e34fbf7bd7
kec <weoncheol1@naver.com>
2024-09-16 01:11:01 +0900

    feat: 상품 그룹 관리 및 최저가 조회 서비스 메서드 구현
    LowestPriceService 인터페이스에 상품 그룹을 추가하고 키워드에 상품 그룹 ID를 연결하는 메서드를 정의하고, 이를 LowestPriceServiceImpl에서 구현한다.

--- src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
+++ src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.pricecompareredis.service;
 
+import com.example.pricecompareredis.vo.Keyword;
 import com.example.pricecompareredis.vo.Product;
+import com.example.pricecompareredis.vo.ProductGrp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -15,18 +19,69 @@ public class LowestPriceServiceImpl implements LowestPriceService{
     private RedisTemplate myProdPriceRedis;
 
     @Override
-    public Set getZsetValue(String key) {
+    public Set GetZsetValue(String key) {
         Set myTempSet = new HashSet();
         myTempSet = myProdPriceRedis.opsForZSet().rangeWithScores(key, 0, 9);
         return myTempSet;
     }
 
     @Override
-    public int setNewProduct(Product newProduct) {
+    public int SetNewProduct(Product newProduct) {
         int rank = 0;
         myProdPriceRedis.opsForZSet().add(newProduct.getProdGrpId(), newProduct.getProductId(), newProduct.getPrice());
         rank = myProdPriceRedis.opsForZSet().rank(newProduct.getProdGrpId(), newProduct.getProductId()).intValue();
 
         return rank;
+    }
+
+    @Override
+    public int SetNewProductGrp(ProductGrp newProductGrp) {
+        List<Product> product = newProductGrp.getProductList();
+        String productId = product.get(0).getProductId();
+        int price = product.get(0).getPrice();
+        myProdPriceRedis.opsForZSet().add(newProductGrp.getProdGrpId(), productId, price);
+        int productCnt  = myProdPriceRedis.opsForZSet().zCard(newProductGrp.getProdGrpId()).intValue();
+
+        return productCnt;
+    }
+
+    public int SetNewProductGrpToKeyword(String keyword, String prodGrpId, double score) {
+        myProdPriceRedis.opsForSet().add(keyword, prodGrpId, score);
+        int rank = myProdPriceRedis.opsForZSet().rank(keyword, prodGrpId).intValue();
+        return rank;
+    }
+
+    @Override
+    public Keyword GetLowestPriceProductByKeyword(String keyword) {
+        Keyword returnInfo = new Keyword();
+        List<ProductGrp> tempProdGrp = new ArrayList<>();
+
+        // keyword 를 통해 ProductGroup 가져오기 (10개)
+        tempProdGrp = GetProdGrpUsingKeyword(keyword);
+
+        // 가져온 정보들을 Return 할 Object 에 넣기
+        returnInfo.setKeyword(keyword);
+        returnInfo.setProductGrpList(tempProdGrp);
+        
+        // 해당 Object return
+        return returnInfo;
+    }
+
+    public List<ProductGrp> GetProdGrpUsingKeyword(String keyword) {
+
+        List<ProductGrp> returnInfo = new ArrayList<>();
+        ProductGrp tempProdGrp = new ProductGrp();
+
+        // input 받은 keyword 로 productGrpId를 조회
+        List<String> prodGrpIdList = new ArrayList<>();
+        prodGrpIdList = List.copyOf(myProdPriceRedis.opsForZSet().range(keyword, 0, 9));
+
+        for (final String prodGrpId : prodGrpIdList) {
+            // Loop 돌면서 ProductGroupID 로 Product:price 가져오기 (10개)
+            Set prodAndPriceList = new HashSet();
+            prodAndPriceList = myProdPriceRedis.opsForZSet().rangeWithScores(prodGrpId, 0, 9);
+        }
+
+        return returnInfo;
     }
 }
