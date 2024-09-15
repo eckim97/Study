063df0ffb7b8ae604265afcdc57abe3947f9ca64
kec <weoncheol1@naver.com>
2024-09-16 00:25:35 +0900

    feat: 상품 정보 관리를 위한 필드 추가
    Product에서 상품 관련 데이터 전송 필드 추가

--- src/main/java/com/example/pricecompareredis/vo/Product.java
+++ src/main/java/com/example/pricecompareredis/vo/Product.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Product {
-
+    private String prodGrpId; // FPG0001
     private String productId; // d1fc1031-da1c-40da-9cd1-e9fef3f2a336
     private int price; // 25000 (won)
 }
