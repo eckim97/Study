066e2b25db544f889e01603a2eb18a0124da8aac
kec <weoncheol1@naver.com>
2024-09-18 04:30:58 +0900

    feat: KafkaConfig 클래스에 스트림 설정 추가
    KafkaConfig: Kafka Stream 설정을 위한 기본 설정 추가, 부트스트랩 서버와 직렬화 클래스 구성

--- src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
+++ src/main/java/com/example/kafkaproducer/config/KafkaConfig.java
@@ -2,12 +2,17 @@ package com.example.kafkaproducer.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
@@ -15,40 +20,52 @@ import java.util.Map;
 
 @Configuration
 @EnableKafka
+@EnableKafkaStreams
 public class KafkaConfig {
 
-    @Bean
-    public KafkaTemplate<String, Object> KafkaTemplateForGeneral() {
-        return new KafkaTemplate<String, Object>(ProducerFactory());
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration myKStreamConfig() {
+        Map<String, Object> myKStreamConfig = new HashMap<>();
+        myKStreamConfig.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-test");
+        myKStreamConfig.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "13.124.237.39:9092,3.38.181.75:9092,3.35.220.144:9092");
+        myKStreamConfig.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        myKStreamConfig.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        myKStreamConfig.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 3);
+        return new KafkaStreamsConfiguration(myKStreamConfig);
     }
 
-    @Bean
-    public ProducerFactory<String, Object> ProducerFactory() {
-        Map<String, Object> myConfig = new HashMap<>();
-        myConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "13.124.237.39:9092,3.38.181.75:9092,3.35.220.144:9092");
-        myConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        myConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-
-        return new DefaultKafkaProducerFactory<>(myConfig);
-    }
-
-    @Bean
-    public ConsumerFactory<String, Object> ConsumerFactory() {
-        Map<String, Object> myConfig = new HashMap<>();
-        myConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "13.124.237.39:9092,3.38.181.75:9092,3.35.220.144:9092");
-        myConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        myConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-
-        return new DefaultKafkaConsumerFactory<>(myConfig);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Object> myfactory = new ConcurrentKafkaListenerContainerFactory<>();
-        myfactory.setConsumerFactory(ConsumerFactory());
-        return myfactory;
-    }
+//    @Bean
+//    public KafkaTemplate<String, Object> KafkaTemplateForGeneral() {
+//        return new KafkaTemplate<String, Object>(ProducerFactory());
+//    }
+//
+//    @Bean
+//    public ProducerFactory<String, Object> ProducerFactory() {
+//        Map<String, Object> myConfig = new HashMap<>();
+//        myConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "13.124.237.39:9092,3.38.181.75:9092,3.35.220.144:9092");
+//        myConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        myConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//
+//
+//        return new DefaultKafkaProducerFactory<>(myConfig);
+//    }
+//
+//    @Bean
+//    public ConsumerFactory<String, Object> ConsumerFactory() {
+//        Map<String, Object> myConfig = new HashMap<>();
+//        myConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "13.124.237.39:9092,3.38.181.75:9092,3.35.220.144:9092");
+//        myConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        myConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//
+//        return new DefaultKafkaConsumerFactory<>(myConfig);
+//    }
+//
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, Object> myfactory = new ConcurrentKafkaListenerContainerFactory<>();
+//        myfactory.setConsumerFactory(ConsumerFactory());
+//        return myfactory;
+//    }
 
 
 }
