17e7e24a4d9c872cea88180f89107c5b657521db
kec <weoncheol1@naver.com>
2024-09-18 17:26:15 +0900

    feat: Kafka Streams 서비스에 외부 조인 기능 추가
    StreamService 클래스에 외부 조인 로직을 추가하여 두 스트림의 키가 일치하지 않는 경우에도 결과를 출력할 수 있도록 개선
    조인 조건과 출력 형식을 업데이트하여 사용자가 결과를 더 명확하게 이해할 수 있게 함

--- src/main/java/com/example/kafkaproducer/service/StreamService.java
+++ src/main/java/com/example/kafkaproducer/service/StreamService.java
@@ -3,10 +3,7 @@ package com.example.kafkaproducer.service;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.JoinWindows;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,23 +22,30 @@ public class StreamService {
 //        myStream.filter((key, value) -> value.contains("freeClass")).to("freeClassList");
 //
         KStream<String, String> leftStream = sb.stream("leftTopic",
-                Consumed.with(STRING_SERDE, STRING_SERDE))
-                .selectKey((k, v) -> v.substring(0, v.indexOf(":")));
+                Consumed.with(STRING_SERDE, STRING_SERDE));
         // key:value --> 1:leftValue
         KStream<String, String> rightStream = sb.stream("rightTopic",
-                Consumed.with(STRING_SERDE, STRING_SERDE))
-                .selectKey((k, v) -> v.substring(0, v.indexOf(":")));
+                Consumed.with(STRING_SERDE, STRING_SERDE));
         // key:value --> 1:rightValue
 
-        leftStream.print(Printed.toSysOut());
-        rightStream.print(Printed.toSysOut());
+        ValueJoiner<String, String, String> stringJoiner = (leftValue, rightValue) -> {
+            return "[StringJoiner]" + leftValue + "-" + rightValue;
+        };
 
-        KStream<String, String> joinStream = leftStream.join
-                (rightStream, (leftValue, rightValue) -> leftValue + "_" + rightValue,
-                        JoinWindows.of(Duration.ofMinutes(1)));
+        ValueJoiner<String, String, String> stringOuterJoiner = (leftValue, rightValue) -> {
+            return "[StringOuterJoiner]" + leftValue + "<" + rightValue;
+        };
+        KStream<String, String> joinStream = leftStream.join(rightStream
+                , (leftValue, rightValue) -> leftValue + "_" + rightValue,
+                JoinWindows.of(Duration.ofMinutes(1)));
+
+        KStream<String, String> outerJoinedStream = leftStream.outerJoin(rightStream,
+                stringOuterJoiner,
+                JoinWindows.of(Duration.ofMinutes(1)));
 
         joinStream.print(Printed.toSysOut());
         joinStream.to("joinedMsg");
+        outerJoinedStream.to("joinedMsg");
         // 1:leftValue_1:rightValue
     }
 }
