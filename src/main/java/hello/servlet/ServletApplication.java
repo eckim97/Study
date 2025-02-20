c30a89d3f829dafa4e80719fd018431edfab78c9
kec <weoncheol1@naver.com>
2025-02-20 22:07:43 +0900

    feat: 스프링 MVC 뷰 리졸버 설정 추가
    - OldController 추가하여 View 조회 가능하도록 설정
    - application.properties에 뷰 리졸버(prefix, suffix) 설정 추가
    - InternalResourceViewResolver를 통한 뷰 반환 기능 구현

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
@@ -11,5 +12,4 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
 }
